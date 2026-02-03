313ebf6ba4837a6badf98b144a556d020685d204
Darron Manning <darronmanning@yahoo.com>
2026-02-03 10:01:19 -0600

    Add number guessing game usin loops and conditionals

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleApp5
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User 1 enter a number between 0 - 100");
+            int secretNumber = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+
+            int guess = 0;
+
+            while (guess != secretNumber)
+            {
+                Console.WriteLine("User 2 guess a number between 0 - 100");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess > secretNumber)
+                {
+                    Console.WriteLine("Your number is too high, guess again");
+                }
+                
+                else if (guess < secretNumber)
+                {
+                    Console.WriteLine("Your number is too low, guess again");
+                }
+                else
+                {
+                    Console.WriteLine("Correct! You guessed the number");
+                }
+            }
+
+        }
+    }
+    
+}
